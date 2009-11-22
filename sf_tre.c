5daf05420bb71893b4f169afd17b9b815d2ef87d
Wu Yongzheng <wuyongzheng@gmail.com>
2009-11-22 22:12:06 +0800

    minor change in tre

--- sf_tre.c
+++ sf_tre.c
@@ -95,13 +95,13 @@ static void dump_tre7 (uint8_t *ptr, int size, int rec_size, struct garmin_tre_m
 	int sdidx = 0;
 	while (size >= rec_size) {
 		printf("%4d:", sdidx ++);
-		printf(" rng2_reloff=0x%x", *(uint32_t *)ptr);
+		printf(" rgn2_reloff=0x%x", *(uint32_t *)ptr);
 		if (rec_size >= 8)
-			printf(" rng3_reloff=0x%x", *(uint32_t *)(ptr+4));
+			printf(" rgn3_reloff=0x%x", *(uint32_t *)(ptr+4));
 		if (rec_size >= 12)
-			printf(" rng4_reloff=0x%x", *(uint32_t *)(ptr+8));
+			printf(" rgn4_reloff=0x%x", *(uint32_t *)(ptr+8));
 		if (rec_size >= 16)
-			printf(" rng5_reloff=0x%x", *(uint32_t *)(ptr+12));
+			printf(" rgn5_reloff=0x%x", *(uint32_t *)(ptr+12));
 		if (rec_size % 4)
 			printf(" flag=0x%x\n", *(ptr+rec_size-1));
 		else
@@ -115,12 +115,17 @@ void dump_tre (struct subfile_struct *tre)
 {
 	struct garmin_tre *header = (struct garmin_tre *)tre->header;
 	struct garmin_tre_map_level *maplevels;
+	int progress = 0;
 
 	assert(tre->typeid == ST_TRE);
 
 	dump_comm(tre->header);
 
 	printf("=== TRE HEADER ===\n");
+
+	if (header->comm.hlen < 0x74)
+		goto headerfini;
+	progress = 0x74;
 	printf("Bound:                  %d(%s) %d(%s) %d(%s) %d(%s)\n",
 			bytes_to_sint24(header->northbound), sint24_to_lat(bytes_to_sint24(header->northbound)),
 			bytes_to_sint24(header->eastbound),  sint24_to_lng(bytes_to_sint24(header->eastbound)),
@@ -148,11 +153,15 @@ void dump_tre (struct subfile_struct *tre)
 	printf("TRE6 Point Overview:    reloff=0x%x absoff=0x%x size=0x%x recsize=0x%x\n",
 			header->tre6_offset, tre->offset + header->tre6_offset, header->tre6_size, header->tre6_rec_size);
 	printf("unknown_070:            %s\n", dump_unknown_bytes(header->unknown_070, sizeof(header->unknown_070)));
-	if (header->comm.hlen <= 0x74)
+
+	if (header->comm.hlen < 0x78)
 		goto headerfini;
+	progress = 0x78;
 	printf("Map ID:                 0x%x(%d)\n", header->mapID, header->mapID);
-	if (header->comm.hlen <= 0x78)
+
+	if (header->comm.hlen < 0x9a)
 		goto headerfini;
+	progress = 0x9a;
 	printf("unknown_078:            %s\n", dump_unknown_bytes(header->unknown_078, sizeof(header->unknown_078)));
 	printf("TRE7:                   reloff=0x%x absoff=0x%x size=0x%x recsize=0x%x\n",
 			header->tre7_offset, tre->offset + header->tre7_offset, header->tre7_size, header->tre7_rec_size);
@@ -160,22 +169,38 @@ void dump_tre (struct subfile_struct *tre)
 	printf("TRE8:                   reloff=0x%x absoff=0x%x size=0x%x recsize=0x%x\n",
 			header->tre8_offset, tre->offset + header->tre8_offset, header->tre8_size, header->tre8_rec_size);
 	printf("unknown_094:            %s\n", dump_unknown_bytes(header->unknown_094, sizeof(header->unknown_094)));
-	if (header->comm.hlen <= 0x9a)
+
+	if (header->comm.hlen < 0xbc)
 		goto headerfini;
+	progress = 0xbc;
 	printf("Encrpytion Key:         %s\n", dump_unknown_bytes(header->key, sizeof(header->key)));
 	printf("TRE9:                   reloff=0x%x absoff=0x%x size=0x%x recsize=0x%x\n",
 			header->tre9_offset, tre->offset + header->tre9_offset, header->tre9_size, header->tre9_rec_size);
 	printf("unknown_0b8:            %s\n", dump_unknown_bytes(header->unknown_0b8, sizeof(header->unknown_0b8)));
-	if (header->comm.hlen <= 0xbc)
+
+	if (header->comm.hlen < 0xca)
 		goto headerfini;
+	progress = 0xca;
 	printf("TRE10:                  reloff=0x%x absoff=0x%x size=0x%x recsize=0x%x\n",
 			header->tre10_offset, tre->offset + header->tre10_offset, header->tre10_size, header->tre10_rec_size);
-	if (header->comm.hlen > sizeof(struct garmin_tre))
-		printf("from 0x%x to 0x%x (0x%x bytes): %s\n",
-				sizeof(struct garmin_tre), header->comm.hlen - 1,
-				header->comm.hlen - sizeof(struct garmin_tre),
-				dump_unknown_bytes((uint8_t *)header + sizeof(struct garmin_tre), header->comm.hlen - sizeof(struct garmin_tre)));
+	printf("unknown_0c6:            %s\n", dump_unknown_bytes(header->unknown_0c6, sizeof(header->unknown_0c6)));
+
+	if (header->comm.hlen < 0xce)
+		goto headerfini;
+	progress = 0xce;
+	printf("unknown_0ca:            %s\n", dump_unknown_bytes(header->unknown_0ca, sizeof(header->unknown_0ca)));
+
+	if (header->comm.hlen < 0xcf)
+		goto headerfini;
+	progress = 0xcf;
+	printf("unknown_0ce:            0x%x\n", header->unknown_0ce);
+
 headerfini:
+	if (header->comm.hlen > progress)
+		printf("from 0x%x to 0x%x (0x%x bytes): %s\n",
+				progress, header->comm.hlen - 1,
+				header->comm.hlen - progress,
+				dump_unknown_bytes((uint8_t *)header + progress, header->comm.hlen - progress));
 
 	printf("=== MAP LEVELS ===\n");
 	if (header->comm.locked) {
